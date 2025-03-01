2480b6c6d7c20d4250ed26e84d6ed7482c5484ee
Cory Forsythe <cory@forsythenetworks.com>
2025-03-01 07:44:03 -0600

    lab example

--- install_node.sh
+++ install_node.sh
@@ -1,9 +1,6 @@
-# Install on ec2
-
-```bash
+#!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 . ~/.bashrc
 source ~/.bashrc
 nvm install v22.14.0
 nvm use v22.14.0
- ```
